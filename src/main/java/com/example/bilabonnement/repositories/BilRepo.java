f00c33f040545ed25f7a8cdff10e8146ffd8930e
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2022-05-06 18:12:22 +0200

    Fiddling with Bil and BilRepo

--- src/main/java/com/example/bilabonnement/repositories/BilRepo.java
+++ src/main/java/com/example/bilabonnement/repositories/BilRepo.java
@@ -2,12 +2,14 @@ package com.example.bilabonnement.repositories;
 
 import com.example.bilabonnement.models.Bil;
 import com.example.bilabonnement.models.brugere.Bruger;
+import com.example.bilabonnement.models.brugere.BrugerType;
 import com.example.bilabonnement.utility.DatabaseConnectionManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BilRepo implements CRUDInterface<Bil> {
@@ -17,7 +19,8 @@ public class BilRepo implements CRUDInterface<Bil> {
     public boolean create(Bil bil) {
         try{
             String sql = "INSERT INTO biler(`bil_stelnummer`, `bil_name`, `bil_model`) " +
-                    "VALUES ('" + bil.getStelNummer() + "', " +
+                    "VALUES (" +
+                    "'" + bil.getStelNummer() + "', " +
                     "'" + bil.getName() + "', " +
                     "'" + bil.getModel() + "');";
 
@@ -36,25 +39,26 @@ public class BilRepo implements CRUDInterface<Bil> {
 
     @Override
     public Bil getSingleEntityById(int id) {
+
         try {
-            String sql = "SELECT * FROM biler WHERE bil_id = '" + id + "';";
+            String sql = "SELECT * FROM biler WHERE bruger_id = " + id + ";";
             Connection conn = DatabaseConnectionManager.getConnection();
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
-            String stelnummer = rs.getString(1);
-            String navn = rs.getString(2);
-            String model = rs.getString(3);
+            while(rs.next()){
 
-            int[] brugeradgang = new int[3];
-            brugeradgang[0] = bruger_type;
+                String stelnummer = rs.getString(1);
+                String navn = rs.getString(2);
+                String model = rs.getString(3);
 
-            return new Bruger(bruger_id, navn, adgangskode, brugeradgang);
+                return new Bruger(stelnummer, navn, model);
+            }
 
         }
         catch (SQLException e){
             e.printStackTrace();
-            System.out.println("Kunne ikke finde bruger med id: " + id);
+            System.out.println("Kunne ikke finde bil med id: " + id);
         }
         return null;
     }
