c076ac6ee4aa15ad003e3f1e3c75b3e17654b027
Christian Sandholdt Trip <90309082+ChristianTrip@users.noreply.github.com>
2022-05-16 11:10:27 +0200

    commited

--- src/main/java/com/example/bilabonnement/controllers/AdminController.java
+++ src/main/java/com/example/bilabonnement/controllers/AdminController.java
@@ -1,6 +1,9 @@
 package com.example.bilabonnement.controllers;
 
+import com.example.bilabonnement.repositories.LejeaftaleRepo;
+import com.example.bilabonnement.services.DataregService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,13 +16,26 @@ public class AdminController {
     Skal kunne se den samlede pris for alle lejeaftaler
      */
 
+    private DataregService dataregService = new DataregService();
+    private LejeaftaleRepo lejeaftaleRepo = new LejeaftaleRepo();
+
     @GetMapping("/admin")
     public String home(){
         return "admin";
     }
 
+    @GetMapping("/alleLejeaftaler")
+    public String alleLejeaftaler(){
+        return "alleLejeaftaler";
+    }
+
+
     @GetMapping("/seAlleLejeaftaler")
-    public String seAlleLejeaftaler(){
+    public String seAlleLejeaftaler(Model model){
+
+        System.out.println(lejeaftaleRepo.getAllEntities());
+        model.addAttribute("lejeaftaler", lejeaftaleRepo.getAllEntities());
+
         return "alleLejeaftaler";
     }
 }
